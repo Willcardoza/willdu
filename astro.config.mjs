5ecb0acb07510386d83fa97171b21abf27405db8
willcdubon <william.cdubon@gmail.com>
2024-04-23 18:02:16 -0600

    Preparado para subir, quedan pocos detalles

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,5 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: {
-    defaultStrategy: 'viewport'
-  },
   integrations: [tailwind(), react()]
 });
